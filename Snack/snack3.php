eca6bfd5690a1d8f1e200713ac587c6803557f68
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-08-23 15:52:31 +0200

    add title

--- Snack/snack3.php
+++ Snack/snack3.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP SNACKS 3</title>
 </head>
 <body>
     <!-- Creare un array con 15 numeri casuali, tenendo conto che l'array non dovrà contenere lo stesso numero più di una volta -->
 
     <section>
+
+        <h1>Php Snack 3</h1>
+
         <?php 
             $array = [];
 
